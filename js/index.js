903ec0fa1ccc7215160f8c17c398cae40b4b7558
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-01-11 00:10:03 +0100

    changement CSS menu deroulant

--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-// const response = await fetch("data/projets.json");
-// const projets = await response.json();
-
-// for (let i = 0; i < projets.length; i++) {
-
-//     const card = projets[i];
-
-//     const sectionCard = document.querySelector(".sectionCard");
-
-//     const projetCard = document.createElement("article");
-
-//     const nomCard = document.createElement("h3");
-//     nomCard.innerText = card.nom;
-
-//     const imageCard = document.createElement("img");
-//     imageCard.src = card.image;
-
-//     const technologiesCard = document.createElement("p");
-//     technologiesCard.innerText = card.technologies;
-
-//     const boutonCard = document.createElement("button");
-//     boutonCard.classList.toggle("button")
-//     boutonCard.innerText = ("voir")
-
-
-//     sectionCard.appendChild(projetCard);
-//     projetCard.appendChild(nomCard);
-//     projetCard.appendChild(imageCard);
-//     projetCard.appendChild(technologiesCard);
-//     projetCard.appendChild(boutonCard);
-// }
-
 /// MENU DEROULANT ///
 
 const hamburgerButton = document.querySelector(".nav-toggler")
@@ -120,7 +88,7 @@ new Swiper('.card-wrapper', {
         0: {
             slidesPerView: 1,
         },
-        768: {
+        468: {
             slidesPerView: 2,
         },
         1024: {
