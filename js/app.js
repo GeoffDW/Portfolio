0e19fefe44c97f072966ca54691d701f767a38b6
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-01-15 00:56:40 +0100

    change gsap js

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ gsap.from(selection[0].chars, {
     scrollTrigger: {
         trigger: ".text-reveal",
         start: "top 70%",
-        end: "bottom 40%",
-        scrub: true,
+        end: "bottom 70%",
+        scrub: 1,
     }
 })
 
@@ -29,10 +29,52 @@ const lenis = new Lenis();
 lenis.on('scroll', ScrollTrigger.update);
 
 gsap.ticker.add((time) => {
-  lenis.raf(time * 3000); 
+  lenis.raf(time * 2700); 
 });
 
 gsap.ticker.lagSmoothing(0);
 
 
+const aProposImg = document.querySelector("#a-propos img")
 
+gsap.matchMedia().add("(min-width: 1024px)", () => {
+
+gsap.to(aProposImg, {
+    xPercent : 200,
+    duration : 2,
+    scrollTrigger : {
+        trigger : aProposImg,
+        start : "top 95%",
+        end : "bottom 70%",
+        scrub : 1,
+    }
+})
+})
+
+gsap.matchMedia().add("(max-width: 768px)", () => {
+
+    gsap.to(aProposImg, {
+        xPercent : 480,
+        scrollTrigger : {
+            trigger : aProposImg,
+            start : "top 95%",
+            end : "bottom 10%",
+            scrub : true,
+        }
+    })
+})
+
+
+const skillsTitle = document.querySelector("#skills h2")
+
+gsap.to(skillsTitle, {
+    opacity: 1,
+    duration: 3,
+    scrollTrigger: {
+        trigger: skillsTitle,
+        start: "top 80%",
+        end: "top 50%",
+        markers: true,
+        scrub: true,
+    }
+})
