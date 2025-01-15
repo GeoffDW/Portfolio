7ba473b49dd3a5eedebbb894bfa17c5cf2aa0431
Geoffroy Martin <geoffroy_martin9580@hotmail.fr>
2025-01-15 15:27:09 +0100

    change smooth scroll

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ const lenis = new Lenis();
 lenis.on('scroll', ScrollTrigger.update);
 
 gsap.ticker.add((time) => {
-    lenis.raf(time * 2700);
+    lenis.raf(time * 2000);
 });
 gsap.ticker.lagSmoothing(0);
 
